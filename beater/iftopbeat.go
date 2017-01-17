6c0c63795763a9a343100ddda5820b25b47fc8a2
Yuemin Li <yuemin.li.zoey@gmail.com>
2017-01-17 09:04:58 -0500

    exec cmd; extract interested lines; go clean

--- beater/iftopbeat.go
+++ beater/iftopbeat.go
@@ -1,7 +1,13 @@
 package beater
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/elastic/beats/libbeat/beat"
@@ -18,6 +24,20 @@ type Iftopbeat struct {
 	client publisher.Client
 }
 
+type IftopEvent struct {
+	Interface string `json:"interface"`
+	Interval  string `json:"interval"`
+	Src       string `json:"source"`
+	Dest      string `json:"destination"`
+	Upload    string `json:"upload"`
+	Download  string `json:"download"`
+}
+
+type Pair struct {
+	Src  string `json:"source"`
+	Dest string `json:"destination"`
+}
+
 // Creates beater
 func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
 	config := config.DefaultConfig
@@ -26,10 +46,66 @@ func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
 	}
 
 	bt := &Iftopbeat{
-		done: make(chan struct{}),
+		done:   make(chan struct{}),
 		config: config,
 	}
 	return bt, nil
+}
+
+func (bt *Iftopbeat) getEvents() ([]IftopEvent, error) {
+	iftop, err := exec.LookPath("iftop")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("iftop is available at %s\n", iftop)
+	interval := 10
+	listenOn := "en0"
+	numLines := 10
+	args := []string{"-t", "-s", strconv.Itoa(interval), "-L", strconv.Itoa(numLines), "-i", listenOn, "-n"}
+	cmd := exec.Command(iftop, args...)
+	log.Print(cmd.Path, cmd.Args)
+
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
+		log.Fatal(err)
+	}
+	lines := strings.Split(out.String(), "\n")
+	var ret []Pair
+	for _, l := range lines {
+		log.Print(l)
+		if len(l) != 0 && strings.Contains(l, "=>") {
+			ret = append(ret, Pair{})
+			ret[len(ret)-1].Src = l
+		} else if len(l) != 0 && strings.Contains(l, "<=") {
+			ret[len(ret)-1].Dest = l
+		}
+	}
+	log.Print(ret)
+
+	events := []IftopEvent{}
+
+	for _, value := range ret {
+		event := IftopEvent{Interval: strconv.Itoa(interval), Interface: listenOn}
+		// TODO(yuemin): use more verbose FieldsFunc
+		log.Print(value)
+		uploadRecord := strings.Fields(value.Src)
+		event.Src = uploadRecord[1]
+		event.Upload = uploadRecord[4]
+		downRecord := strings.Fields(value.Dest)
+		event.Dest = downRecord[0]
+		event.Download = downRecord[3]
+
+		output, _ := json.Marshal(event)
+		log.Print(string(output))
+		events = append(events, event)
+	}
+	return events, nil
 }
 
 func (bt *Iftopbeat) Run(b *beat.Beat) error {
@@ -45,14 +121,29 @@ func (bt *Iftopbeat) Run(b *beat.Beat) error {
 		case <-ticker.C:
 		}
 
-		event := common.MapStr{
-			"@timestamp": common.Time(time.Now()),
-			"type":       b.Name,
-			"counter":    counter,
+		events, err := bt.getEvents()
+		if err != nil {
+			log.Fatal(err)
 		}
-		bt.client.PublishEvent(event)
-		logp.Info("Event sent")
-		counter++
+		for _, record := range events {
+			event := common.MapStr{
+				"@timestamp": common.Time(time.Now()),
+				"type":       b.Name,
+				"counter":    counter,
+				"event": common.MapStr{
+					"interface":   record.Interface,
+					"interval":    record.Interval,
+					"source":      record.Src,
+					"destination": record.Dest,
+					"upload":      record.Upload,
+					"download":    record.Download,
+				},
+			}
+			bt.client.PublishEvent(event)
+			logp.Info("Event sent")
+			counter++
+		}
+
 	}
 }
 
